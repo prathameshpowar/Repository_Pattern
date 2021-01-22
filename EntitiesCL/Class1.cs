902c193c0bb781e3ea7ba5cc10a595cfc3f94b77
Prathamesh <powarprathamesh123@gmail.com>
2021-01-22 11:18:34 +0530

    initial commit

---
+++ EntitiesCL/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EntitiesCL
+{
+    public class Class1
+    {
+    }
+
+    public class Institution
+    {
+        public long InstitutionID { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Course
+    {
+        public long CourseID { get; set; }
+        public long InstitutionID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+}
---
+++ ServicesCL/Class1.cs
@@ -0,0 +1,49 @@
+﻿using EntitiesCL;
+using System;
+
+namespace ServicesCL
+{
+    public class Class1
+    {
+    }
+
+    public interface IInstututionService
+    {
+        Institution GetInstitutionByID(long institutionID);
+    }
+
+    public interface ICourseService
+    {
+        Course GetCourseByID(long courseID);
+    }
+
+
+    public class InstitutionService : IInstututionService
+    {
+        public Institution GetInstitutionByID(long institutionID)
+        {
+            return new Institution()
+            {
+                InstitutionID = institutionID,
+                Name = "Demo Institution",
+                Address = "Demo Address",
+                Telephone = "0123456789"
+            };
+        }
+    }
+
+    public class CourseService : ICourseService
+    {
+        public Course GetCourseByID(long courseID)
+        {
+            return new Course
+            {
+                CourseID = courseID,
+                Description = "Demo course description",
+                InstitutionID = 1,
+                Title = "Demo Course Title"
+            };
+        }
+    }
+
+}
